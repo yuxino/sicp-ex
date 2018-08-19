f647463d44d937022bf60169c1841dc8040bc812
-A- <nbsawz@gmail.com>
2018-08-20 00:16:11 +0800

    add t.rkt

---
+++ ex-1/t.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+; code blow
+
+(define (square x)
+  (* x x)
+)
+
+(define (sum-of-squares x y)
+  (
+    + (square x) (square y)
+  )
+)
+
+(define (f a)
+  (
+    sum-of-squares (+ a 1) (* a 2)
+  )
+)
+
+(f 5)
