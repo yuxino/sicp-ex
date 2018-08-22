ae13146d21c3bf1bc05fd1624476383162b8984d
-A- <nbsawz@gmail.com>
2018-08-22 23:34:10 +0800

    add ex-1.3

--- ex-1/1.3.rkt
+++ ex-1/1.3.rkt
@@ -2,3 +2,11 @@
 
 ; code blow
 
+
+(define (bigger-sum-of x y z) (
+  cond ((and (< x y) (< x z)) (+ y z))
+       ((and (< y x) (< y z)) (+ x z))
+       (else (+ x y)) 
+))
+
+(bigger-sum-of 1 2 3)
