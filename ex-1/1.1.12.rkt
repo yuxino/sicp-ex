41fa3aa6327c89a9572c93da890e7b4a096b3d4f
yuxino <nbsawz@gmail.com>
2019-08-05 02:07:55 +0800

    pascal triangle

---
+++ ex-1/1.1.12.rkt
@@ -0,0 +1,13 @@
+ #lang racket
+
+; 帕斯卡三角
+
+(define (pascal row col)
+    (cond ((> col row)
+            (error "unvalid col value"))
+          ((or (= col 0) (= row col))
+            1)
+          (else (+ (pascal (- row 1) (- col 1))
+                   (pascal (- row 1) col)))))
+
+(pascal 4 2)
