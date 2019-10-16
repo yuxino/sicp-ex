81888282ecd2508c9b0ed06c3d15a38c1b700ebf
yuxino <nbsawz@gmail.com>
2019-10-17 01:04:36 +0800

    add map

--- ex-2/map.rkt
+++ ex-2/map.rkt
@@ -1,1 +1,13 @@
-; wait to implement
+#lang racket
+
+(define (map proc items)
+  (if (null? items)
+    items
+    (cons
+      (proc (car items))
+      (map proc (cdr items))
+    )
+  )
+)
+
+(map (lambda (x) (* x x)) (list 1 2 3 4))
