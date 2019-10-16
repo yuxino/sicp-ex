cf49b9af2dfed9e6448ee630662695e875d60659
yuxino <nbsawz@gmail.com>
2019-10-17 00:42:40 +0800

    add 2.18, ahhhh i am sutpid man ...

--- ex-2/2.18.rkt
+++ ex-2/2.18.rkt
@@ -1,19 +1,34 @@
 #lang racket
 
-; (reverse (list 1 2 3))
-; (3 2 1)
-
-(define (reverse list)
-  (define (reverse-ret items n)
-    (if (= n 1)
-      items
-      (reverse-ret (cdr items) (- n 1))
-    )
+; 我实现的。。。
+(define (list-ref items n)
+  (
+    if (= n 1)
+       (car items)
+       (list-ref (cdr items) (- n 1))
   )
-  (reverse-ret list (length list))
-
-  ;
-  ; (cons (cons 2 3) 1)
 )
 
+(define (reverse list)
+  (define (reverse-iter items n l)
+    (if (= (- l n) 1)
+      (cons (list-ref items 1) '())
+      (cons (
+        list-ref items (- l n))
+        (reverse-iter items (+ n 1) l)
+      )
+    )
+  )
+  (reverse-iter list 0 (length list))
+)
+
+; 草 别人写的
+; (define (reverse l)
+;   (define (iter l l0)
+;     (if (= (length l) 0)
+;         l0
+;         (iter (cdr l) (cons (car l) l0))))
+;   (iter l '()))
+
 (reverse (list 1 2 3 4))
+(reverse (list 4 3 2 1))
