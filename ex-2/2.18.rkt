e38b8fd4e105b34f7344fbdf4f7e3c5a34a367aa
yuxino <nbsawz@gmail.com>
2019-10-20 23:13:50 +0800

    fuck i can't think anything ..

--- ex-2/2.18.rkt
+++ ex-2/2.18.rkt
@@ -1,34 +1,35 @@
 #lang racket
 
 ; 我实现的。。。
-(define (list-ref items n)
-  (
-    if (= n 1)
-       (car items)
-       (list-ref (cdr items) (- n 1))
-  )
-)
+; (define (list-ref items n)
+;   (
+;     if (= n 1)
+;        (car items)
+;        (list-ref (cdr items) (- n 1))
+;   )
+; )
 
-(define (reverse list)
-  (define (reverse-iter items n l)
-    (if (= (- l n) 1)
-      (cons (list-ref items 1) '())
-      (cons (
-        list-ref items (- l n))
-        (reverse-iter items (+ n 1) l)
-      )
-    )
-  )
-  (reverse-iter list 0 (length list))
-)
+; (define (reverse list)
+;   (define (reverse-iter items n l)
+;     (if (= (- l n) 1)
+;       (cons (list-ref items 1) '())
+;       (cons (
+;         list-ref items (- l n))
+;         (reverse-iter items (+ n 1) l)
+;       )
+;     )
+;   )
+;   (reverse-iter list 0 (length list))
+; )
 
 ; 草 别人写的
-; (define (reverse l)
-;   (define (iter l l0)
-;     (if (= (length l) 0)
-;         l0
-;         (iter (cdr l) (cons (car l) l0))))
-;   (iter l '()))
+(define (reverse l)
+  (define (iter l l0)
+    (if (= (length l) 0)
+        l0
+        (iter (cdr l) (cons (car l) l0))))
+  (iter l '()))
 
 (reverse (list 1 2 3 4))
 (reverse (list 4 3 2 1))
+(reverse (list 3))
--- ex-2/2.26.rkt
+++ ex-2/2.26.rkt
@@ -1,2 +1,31 @@
 #lang racket
 
+; 翻转树
+
+(define (reverse l)
+  (define (iter l l0)
+    (if (= (length l) 0)
+        l0
+        (iter (cdr l) (cons (car l) l0))))
+  (iter l '()))
+
+(define (fuck-pair items)
+  (cond
+    ((not (pair? items)) items)
+    (else (reverse items))
+  )
+)
+
+(define (reverse-tree items)
+  'EOF
+)
+
+; (reverse-tree (cons (list 1 2) (list 3 4)))
+
+; (reverse-tree (list 1))
+
+; (reverse-tree (list 3 4))
+
+(reverse-tree (cons (list 1) (list 2)))
+
+; (cons (list 1 2) (list 3 4))
