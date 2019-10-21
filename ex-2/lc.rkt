d01faf5934dbc4c8513fcd1eba1bb5e10bf628f3
yuxino <nbsawz@gmail.com>
2019-10-21 18:32:35 +0800

    deep in list and cons

--- ex-2/lc.rkt
+++ ex-2/lc.rkt
@@ -5,13 +5,41 @@
 ; (list 1 2) = (1 2)
 ; (list 1 2) = (cons 1 (cons 2 ()))
 
+(define empty '())
 (display (list 1 2))
 (newline)
 (display (cons 1 2))
 (newline)
-(display (cons 1 (cons 2 '())))
+(display (cons 1 (cons 2 empty)))
 (newline)
+
 (display (list 1 2 (list 3 4)))
 (newline)
-(display (list 1 2 (cons 3 (cons 4 '()))))
+(display (list 1 2 (cons 3 (cons 4 empty))))
+(newline)
+(
+  display
+  (cons 1 
+    (cons 2 
+      (cons 
+        (cons 3
+          (cons 4 empty)
+        )
+        empty
+      )
+    )
+  )
+)
+(newline)
+(
+  display
+  (cons 
+    (cons 4 
+      (cons 3 empty)
+    )
+    (cons 2
+      (cons 1 empty)
+    )
+  )
+)
 (newline)
