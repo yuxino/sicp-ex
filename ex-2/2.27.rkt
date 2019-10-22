51556d2b9dfbc1169a7f2afbf45dbe3482d46481
yuxino <nbsawz@gmail.com>
2019-10-22 22:54:37 +0800

    reanme ex

--- ex-2/2.27.rkt
+++ ex-2/2.27.rkt
@@ -1,50 +1,60 @@
 #lang racket
-; 让树变得平滑 输出结果
-; 输入 (1 2 (3 (4 5)))
-; 输出 (1 2 3 4 5)
-(require racket/trace)
+
+; 翻转树 标准答案
+; http://community.schemewiki.org/?sicp-ex-2.27
+
+ ;; A value for testing. 
+ (define x (list (list 1 (list 2 1)) (list 3 4)))
+
+ ;; My environment doesn't have nil. 
+ (define nil '()) 
+
+ ;; Here's reverse for reference: 
+ (define (reverse items) 
+   (define (rev-imp items result) 
+     (if 
+        (null? items) result 
+        (rev-imp (cdr items) (cons (car items) result))
+     )
+   )
+   (rev-imp items nil)
+)
 
 (define (append list1 list2)
   (
     if (null? list1)
     list2
     (
-      cons (car list1) 
-           (append (cdr list1) list2)
+      cons (car list1) (append (cdr list1) list2)
     )
   )
 )
 
-; 借助 append 的版本
-(define (fringe items)
+(define (deep-reverse li) 
   (cond 
-    ((null? items) '())
-    ((not (pair? items)) (list items))
-    (else
-      (append (fringe (car items))
-              (fringe (cdr items))
+    ((null? li) '()) 
+    ; 匹配到值  -> 结束
+    ((not (pair? li)) li) 
+    ; 只处理列表 (这边不处理序对的情况)
+    ; 因为当是序对的时候 要考虑特殊情况
+    ; 如果是序对的话
+    ; append 左值将不是列表。
+    ; 但是如果你让他强行变成列表又会出现新的问题 ...
+    ; 会变的死循环
+    ; 感觉有点儿无解 ....
+    (else 
+      (append 
+        ; 右边
+        (deep-reverse (cdr li))  
+        ; 左边归档成列表
+        (list (deep-reverse (car li)))
       )
     )
   )
 )
 
-; (1 2 3 7 8 9)
-(fringe (list 1 2 3 (list 7 8 9)))
-; (5 6 7 2 3 4)
-(fringe (list (list (list 5) 6 7) 2 (cons 3 4)))
-; (5 6 7 2 3 4)
-(fringe (list (list (list 5) 6 7) 2 (list 3 4)))
+ (deep-reverse (list 1 2))
+;  (deep-reverse (list 1 2 (list 3 4)))
+;  (deep-reverse (list 1 (cons 2 3)))
 
-; 不用 append 的版本
-; 噢噢噢 看了答案之后思路就是 当找到值的
-; 时候就可以归档了 往上面回退回去 ~
-
-; 看到的答案
-;  (define (fringe tree) 
-;    (define nil '()) 
-;    (define (build-fringe x result) 
-;      (cond ((null? x) result) 
-;            ((not (pair? x)) (cons x result)) 
-;            (else (build-fringe (car x)  
-;                                (build-fringe (cdr x) result))))) 
-;    (build-fringe tree nil)) 
+; (car (cdr (cons 1 2)))
---
+++ ex-2/2.28.rkt
@@ -0,0 +1,51 @@
+#lang racket
+; 让树变得平滑 输出结果
+; 输入 (1 2 (3 (4 5)))
+; 输出 (1 2 3 4 5)
+(require racket/trace)
+
+(define (append list1 list2)
+  (
+    if (null? list1)
+    list2
+    (
+      cons (car list1) 
+           (append (cdr list1) list2)
+    )
+  )
+)
+
+; 借助 append 的版本
+(define (fringe items)
+  (cond 
+    ((null? items) '())
+    ((not (pair? items)) (list items))
+    (else
+      (append (fringe (car items))
+              (fringe (cdr items))
+      )
+    )
+  )
+)
+
+; (1 2 3 7 8 9)
+(fringe (list 1 2 3 (list 7 8 9)))
+; (5 6 7 2 3 4)
+(fringe (list (list (list 5) 6 7) 2 (cons 3 4)))
+; (5 6 7 2 3 4)
+(fringe (list (list (list 5) 6 7) 2 (list 3 4)))
+
+; 不用 append 的版本
+; 噢噢噢 看了答案之后思路就是 当找到值的
+; 时候就可以归档了 往上面回退回去 ~
+; 有空再写执行轨迹吧 //
+
+; 看到的答案
+;  (define (fringe tree) 
+;    (define nil '()) 
+;    (define (build-fringe x result) 
+;      (cond ((null? x) result) 
+;            ((not (pair? x)) (cons x result)) 
+;            (else (build-fringe (car x)  
+;                                (build-fringe (cdr x) result))))) 
+;    (build-fringe tree nil)) 
